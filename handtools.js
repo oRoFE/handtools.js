8b153c07f1f267c6ba1fc66e39025882017df38a
Shinya Katayama <shinya.katayama@jp.oro.com>
2015-07-02 09:51:48 +0900

    add template.

---
+++ handtools.js
@@ -0,0 +1,18 @@
+;(function($) {
+
+  $.handtools = (function() {
+
+    /**
+     * Public functions
+     */
+
+    /**
+     * Private functions
+     */
+
+    return {
+
+    };
+  })();
+
+})(jQuery);
